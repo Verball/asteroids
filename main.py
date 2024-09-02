77eed091dbf652662160ac460a0ff88446043fb6
Verball <chavezchristian50@gmail.com>
2024-09-01 21:55:01 -0700

    Finish Player class: Allowing movement and roation

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    dt = 0
+    
     while (1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for obj in updatable:
+            obj.update(dt)
+    
         pygame.Surface.fill(screen, (0,0,0))
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
+        
+        #Limit Frame to 60FPS
+        dt = fps.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
